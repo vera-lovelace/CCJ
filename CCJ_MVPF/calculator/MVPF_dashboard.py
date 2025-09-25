e423da696ab086813ea18536114821d6bd530b91
vera-lovelace <adrienn.juhasz@email.com>
2025-09-25 16:14:22 -0700

    python code for dashboard added

---
+++ CCJ_MVPF/calculator/MVPF_dashboard.py
@@ -0,0 +1,13 @@
+import plotly
+import pandas
+import numpy as np
+
+from plotly import dash
+
+
+
+# load R script as calculator
+
+# create a dashboard framework
+# features: slides for weights and selected subcomponents, filters for subcomponents, result MVPF calculated
+# Any charts / sensitivity graphs?
