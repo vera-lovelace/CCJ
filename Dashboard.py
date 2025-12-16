d6d8d60e18e54bc9dfd4713f25a118107269ab49
vera-lovelace <adrienn.juhasz@email.com>
2025-12-15 23:24:45 -0800

    dash update to production ready start

--- Dashboard.py
+++ Dashboard.py
@@ -3525,4 +3525,8 @@ register_callbacks(app)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    import os
+
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=False)
+
